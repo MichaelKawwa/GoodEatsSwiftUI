0a2d58517cc9b06e73661ba8a8cc249d8fba3717
MichaelKawwa <kawwamichael@gmail.com>
2020-09-18 20:09:19 -0400

    Initial Commit

---
+++ GoodEatsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodEatsSwiftUI
+//
+//  Created by Michael Kawwa on 9/18/20.
+//  Copyright © 2020 Michael Kawwa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
